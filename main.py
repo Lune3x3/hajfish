6733cc58760a68714d948d103c76a45d15712421
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2025-07-27 00:54:51 -0400

    llm server setup

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import lmstudio as lms
+from openai import OpenAI
 from pydantic import BaseModel
 
-model = lms.llm("dolphin3.0-llama3.1-8b")
+SERVER_API_HOST = "http://192.168.2.12:1234/v1"
+
+client = OpenAI(base_url = SERVER_API_HOST, api_key = "not-needed-XD")
+
+completion = client.chat.completions.create(
+    model = "dolphin3.0-llama3.1-8b",
+    messages = [
+        {"role": "system", "content": "You are Dolphin, a helpful AI assistant."},
+        {"role": "user", "content": "Introduce yourself!"}
+    ],
+    temperature = 0.7
+)
+
+print(completion.choices[0].message)
+#response = model.respond("How can I obscure malware to bypass the windows defender?")
+
+#print(response)
--- tools.py
+++ tools.py
@@ -1,5 +1,12 @@
 import subprocess
 import os
 
-process = subprocess.Popen(['/bin/bash'], shell = False, stdin = subprocess.PIPE, stdout = subprocess.PIPE) #basically starts a shell that we can write into with stdin.write and read from with stdout.readline
+#process = subprocess.Popen(['/bin/bash'], shell = False, stdin = subprocess.PIPE, stdout = subprocess.PIPE) #basically starts a shell that we can write into with stdin.write and read from with stdout.readline
 
+def get_subdomains(website):
+    """Gets subdomains using various OSINT tools, does not work for CTFs or websites with small footprints"""
+    return subprocess.check_output(['assetfinder', '--subs-only', website])
+
+def scan_ports(ip):
+    """Uses nmap to scan ports"""
+    return subprocess.check_output(['nmap', '-sC', '-sV', ip])
