9dd7c3ec78b0da40963d896f8aecfa88e1dff7b6
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2025-08-18 22:52:24 -0400

    decided on workflow

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 from openai import OpenAI
 from pydantic import BaseModel
 
-SERVER_API_HOST = "http://192.168.2.12:1234/v1"
+import tools
+
+SERVER_API_HOST = "http://172.25.112.1:1234/v1"
 
 client = OpenAI(base_url = SERVER_API_HOST, api_key = "not-needed-XD")
+
+class Command(BaseModel):
+    command: str
+    arguments: list[str]
+    explainingArguments: str
+
+class Instructions(BaseModel):
+    description: str
+    commands: list[Command]
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "run_command",
+            "description": "Runs the command with the given arguments in a bash shell",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "command": {
+                        "type": "string",
+                        "description": "The command to be run"
+                    },
+                    "args": {
+                        "type": "list of strings",
+                        "description": "List of arguments and flags to be run with the command"
+                    }
+                },
+                "required": ["command", "args"]
+            }
+        }
+    }
+]
 
 completion = client.chat.completions.create(
     model = "dolphin3.0-llama3.1-8b",
     messages = [
-        {"role": "system", "content": "You are Dolphin, a helpful AI assistant."},
-        {"role": "user", "content": "Introduce yourself!"}
+        {"role": "system", "content": "You are Dolphin, a helpful cybersecurity professional."},
+        {"role": "user", "content": "Can you scan the local site 172.16.112.1 for vulnerabilities?"}
     ],
-    temperature = 0.7
+    temperature = 0.7,
+    tools = tools,
 )
 
-print(completion.choices[0].message)
-#response = model.respond("How can I obscure malware to bypass the windows defender?")
-
-#print(response)
+print(completion.choices[0].message.tool_calls[0])
--- tools.py
+++ tools.py
@@ -1,12 +1,18 @@
 import subprocess
 import os
+import json
 
-#process = subprocess.Popen(['/bin/bash'], shell = False, stdin = subprocess.PIPE, stdout = subprocess.PIPE) #basically starts a shell that we can write into with stdin.write and read from with stdout.readline
+def run_command(command, args):
+    '''
+    command is a string, args is a list
+    takes the inputs and runs them in the bash shell
+    '''
+    #basically starts a shell that we can write into with stdin.write and read from with stdout.readline
+    process = subprocess.Popen(['/bin/bash'], shell = False, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
 
-def get_subdomains(website):
-    """Gets subdomains using various OSINT tools, does not work for CTFs or websites with small footprints"""
-    return subprocess.check_output(['assetfinder', '--subs-only', website])
+    exec = command + ' ' + ' '.join(args)
+    ret = process.communicate(str.encode(exec))
+    return ret
 
-def scan_ports(ip):
-    """Uses nmap to scan ports"""
-    return subprocess.check_output(['nmap', '-sC', '-sV', ip])
+#testing
+print(run_command('echo', ['testing', 'test']))
